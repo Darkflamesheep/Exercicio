4426a6463788f45324ad820ffd5d6fec66e2c84f
pedro aluan <pedroaluan1@outlook.com>
2019-01-18 23:52:37 -0200

    exercicio

---
+++ Exercicio01/Projeto01/Controllers/ProfessorControle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto01.Entities;
+using System.IO;
+
+namespace Projeto01.Controllers
+{
+    public class ProfessorControle
+    {
+        public void ExportarParaTxt(Professor professor )
+        {
+            StreamWriter sw = new StreamWriter("c:\\temp\\professorinformação.txt", true);
+
+            sw.WriteLine("IDPROFESSOR.........:"+ professor.IdProfessor);
+            sw.WriteLine("NOME DO PROFESSOR...:" + professor.Nome);
+            sw.WriteLine("Email...............:" + professor.Email);
+
+            sw.Close();
+        }
+
+    }
+}
---
+++ Exercicio01/Projeto01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto01.Controllers;
+using Projeto01.Entities;
+
+namespace Projeto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Professor professor = new Professor();
+            Console.WriteLine("\nDADOS DO PROFESSOR:\n");
+
+            Console.WriteLine("IdProfessor:");
+            professor.IdProfessor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome Professor:");
+            professor.Nome = Console.ReadLine();
+
+            Console.WriteLine("Email do professor:");
+            professor.Email = Console.ReadLine();
+
+
+            ProfessorControle professorControle = new ProfessorControle();
+            professorControle.ExportarParaTxt(professor);
+
+            Console.WriteLine("Dados gravados");
+
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
